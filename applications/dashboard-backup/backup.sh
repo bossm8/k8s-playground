cd03a1cc92b38290b617d90b667e429af79d1a1f
Marco Boss <marco.boss@switch.ch>
2025-03-15 22:24:03 +0100

    [grafana] fix git config in backup script

--- applications/dashboard-backup/backup.sh
+++ applications/dashboard-backup/backup.sh
@@ -7,8 +7,8 @@ slugify() {
 
 echo "level=info msg=starting backup of dashboards tagged with ${BACKUP_DASHBOARD_TAG}"
 
-git config --user.name "Grafana Backup"
-git config --user.email "grafana-backup@mcathome.ch"
+git config user.name "Grafana Backup"
+git config user.email "grafana-backup@mcathome.ch"
 
 git clone https://oauth2:${BACKUP_GIT_TOKEN}@${BACKUP_GIT_REPO} -o grafana-backup
 
