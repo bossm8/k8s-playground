893d07d72cba21ab146e4dca78697fe5482b66ec
Marco Boss <marco.boss@switch.ch>
2025-04-01 16:44:49 +0200

    [vector] define nova metrics

--- infrastructure/updates/nova/find.sh
+++ infrastructure/updates/nova/find.sh
@@ -7,7 +7,7 @@
 
 # Needs Vector HTTP Server Source
 # https://vector.dev/docs/reference/configuration/sources/http_server/
-# wget \
-#   --post-file="report.json" \
-#   --header="Content-Type: application/json" \
-#   ${VECTOR_HTTP_SERVER_RECEVIER}
+wget \
+  --post-file="report.json" \
+  --header="Content-Type: application/json" \
+  ${NOVA_VECTOR_JSON_ENDPOINT}
