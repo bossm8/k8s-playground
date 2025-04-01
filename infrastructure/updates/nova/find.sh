a4691d32ba6ade0c730bfef8e4767a6b841bb423
Marco Boss <marco.boss@switch.ch>
2025-04-01 15:25:57 +0200

    [nova] create cronjob

---
+++ infrastructure/updates/nova/find.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+/nova find \
+  --helm \
+  --containers \
+  --output-file report.json
+
+# Needs Vector HTTP Server Source
+# https://vector.dev/docs/reference/configuration/sources/http_server/
+# wget \
+#   --post-file="report.json" \
+#   --header="Content-Type: application/json" \
+#   ${VECTOR_HTTP_SERVER_RECEVIER}
