4cc8ccae0b4aec03996c120f596d1686267490be
Marco Boss <marco.boss@switch.ch>
2025-04-01 17:01:13 +0200

    [nova] set output to stdout in wget

--- infrastructure/updates/nova/find.sh
+++ infrastructure/updates/nova/find.sh
@@ -7,7 +7,7 @@
 
 # Needs Vector HTTP Server Source
 # https://vector.dev/docs/reference/configuration/sources/http_server/
-wget \
+wget -O- \
   --post-file="report.json" \
   --header="Content-Type: application/json" \
   ${NOVA_VECTOR_JSON_ENDPOINT}
