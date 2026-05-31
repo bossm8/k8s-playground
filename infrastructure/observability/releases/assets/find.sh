69552c596c9086220ed241f06c95dfe00e475dbb
Boss Marco <bossm8@hotmail.com>
2026-05-31 09:06:06 +0200

    Add patches, configs and jobs

---
+++ infrastructure/observability/releases/assets/find.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+/nova find \
+  --helm \
+  --containers \
+  --output-file report.json
+
+# Needs Vector HTTP Server Source
+# https://vector.dev/docs/reference/configuration/sources/http_server/
+wget -O- \
+  --post-file="report.json" \
+  --header="Content-Type: application/json" \
+  ${NOVA_VECTOR_JSON_ENDPOINT}
